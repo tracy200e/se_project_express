9394f4d2b887fe9078eae58d78c23a7ab99e7f3e
Tracy Chen <tracy200e@gmail.com>
2024-04-18 21:45:10 +1000

    add: middleware for handling unknown route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
+const { DOCUMENT_NOT_FOUND_ERROR } = require("../utils/errors");
 
 const itemRouter = require("./clothingItem");
 const userRouter = require("./users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
+router.use((req, res) => {
+    res.status(DOCUMENT_NOT_FOUND_ERROR).send({ message: "Router not found."});
+})
 
 module.exports = router;
