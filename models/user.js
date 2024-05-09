a9ada768baf2becac9ebd0e4dc907f3dd53ebd3e
Tracy Chen <tracy200e@gmail.com>
2024-05-09 14:32:07 +1000

    add: email and password fields

--- models/user.js
+++ models/user.js
@@ -18,6 +18,20 @@ const userSchema = new mongoose.Schema({
         message: "You must enter a valid URL"
     }
   },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "Wrong email format"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  }
 });
 
 module.exports = mongoose.model("user", userSchema);
