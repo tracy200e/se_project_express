58e959e011a7f787e247b9530498045ab84c36fb
Tracy Chen <tracy200e@gmail.com>
2024-05-09 17:00:44 +1000

    update: added the email and hashed password field data to the createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const { CAST_ERROR, DOCUMENT_NOT_FOUND_ERROR, INTERNAL_SERVER_ERROR } = require("../utils/errors");
+const bcrypt = require("bcryptjs");
 
 // GET all users
 const getUsers = (req, res) => {
@@ -31,16 +32,26 @@ const getUser = (req, res) => {
 
 // CREATE user
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+  const { name, avatar, email, password } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name: name,
+      avatar: avatar,
+      email: email,
+      password: hash,
+    }))
+    .then((user) => res.status(201).send({
+      _id: user._id,
+      email: user.email,
+    }))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
         res.status(CAST_ERROR).send({ message: "Invalid data." });
+      } else if (err.code === 11000) {
+        res.status(DUPLICATE_KEY_ERROR).send({ message: "Duplicate key error. User email already exists!"});
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ messgae: "An error has occurred on the server." });
+        res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
       }
     });
 };
