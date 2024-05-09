ffcb695b6a2b10b0ab4bc48b4ed6960a6ebaedf3
Tracy Chen <tracy200e@gmail.com>
2024-05-09 19:49:45 +1000

    create: the login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,11 @@
 const User = require("../models/user");
-const { CAST_ERROR, DOCUMENT_NOT_FOUND_ERROR, INTERNAL_SERVER_ERROR } = require("../utils/errors");
+const {
+  CAST_ERROR,
+  DOCUMENT_NOT_FOUND_ERROR,
+  INTERNAL_SERVER_ERROR,
+} = require("../utils/errors");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // GET all users
 const getUsers = (req, res) => {
@@ -8,7 +13,9 @@ const getUsers = (req, res) => {
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -25,7 +32,9 @@ const getUser = (req, res) => {
       } else if (err.name === "CastError") {
         res.status(CAST_ERROR).send({ message: "Invalid data." });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
       }
     });
 };
@@ -33,27 +42,58 @@ const getUser = (req, res) => {
 // CREATE user
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name: name,
-      avatar: avatar,
-      email: email,
-      password: hash,
-    }))
-    .then((user) => res.status(201).send({
-      _id: user._id,
-      email: user.email,
-    }))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name: name,
+        avatar: avatar,
+        email: email,
+        password: hash,
+      }),
+    )
+    .then((user) =>
+      res.status(201).send({
+        _id: user._id,
+        email: user.email,
+      }),
+    )
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
         res.status(CAST_ERROR).send({ message: "Invalid data." });
       } else if (err.code === 11000) {
-        res.status(DUPLICATE_KEY_ERROR).send({ message: "Duplicate key error. User email already exists!"});
+        res
+          .status(DUPLICATE_KEY_ERROR)
+          .send({ message: "Duplicate key error. User email already exists!" });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
       }
     });
 };
 
-module.exports = { getUsers, createUser, getUser };
+const logInUser = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password).then((user) => {
+    const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.send({ token });
+
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+      return user;
+    });
+  })
+  .catch((err) => {
+    res.status(401).send({ message: err.message});
+  });
+};
+
+module.exports = { getUsers, createUser, getUser, logInUser };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { getUsers, createUser, getUser } = require("../controllers/users");
+const { getUsers, createUser, getUser, logInUser } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
 router.post("/", createUser);
+router.post("/login", logInUser);
 
 module.exports = router;
