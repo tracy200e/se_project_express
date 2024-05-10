fb8566a36556d60d6df857ac1797183b2c95a638
Tracy Chen <tracy200e@gmail.com>
2024-05-10 13:35:25 +1000

    update: configure user rights

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,5 +1,11 @@
 const ClothingItem = require("../models/clothingItem");
-const { CAST_ERROR, DOCUMENT_NOT_FOUND_ERROR, INTERNAL_SERVER_ERROR } = require("../utils/errors");
+const {
+  CAST_ERROR,
+  DOCUMENT_NOT_FOUND_ERROR,
+  INTERNAL_SERVER_ERROR,
+  FORBIDDEN,
+} = require("../utils/errors");
+const { PROCESSED } = require("../utils/successes");
 
 // Create clothing item
 const createItem = (req, res) => {
@@ -18,7 +24,9 @@ const createItem = (req, res) => {
       if (err.name === "ValidationError") {
         res.status(CAST_ERROR).send({ message: "Invalid data." });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
       }
     });
 };
@@ -29,7 +37,9 @@ const getItem = (req, res) => {
     .then((items) => res.status(200).send(items))
     .catch((err) => {
       console.error(err);
-      res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+      res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -41,14 +51,23 @@ const deleteItem = (req, res) => {
 
   ClothingItem.findByIdAndDelete(itemId)
     .orFail()
-    .then((item) => res.status(CAST_ERROR).send({ data: item }))
+    .then((item) => {
+      if (String(item.owner) !== req.user._id) {
+        return res
+          .status(FORBIDDEN)
+          .send({ message: "This item does not belong to you." });
+      }
+      res.status(PROCESSED).send({ data: item });
+    })
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
         res.status(DOCUMENT_NOT_FOUND_ERROR).send({ message: err.message });
       } else if (err.name === "CastError") {
         res.status(CAST_ERROR).send({ message: "Invalid data." });
       } else {
-      res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
       }
     });
 };
@@ -56,29 +75,10 @@ const deleteItem = (req, res) => {
 const likeItem = (req, res) => {
   ClothingItem.findByIdAndUpdate(
     req.params.itemId,
-    { $addToSet: { likes: req.user._id }},
-    { new: true }
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
   )
-  .orFail()
-  .then((item) => res.status(200).send({ data: item }))
-  .catch((err) => {
-    if (err.name === "DocumentNotFoundError") {
-      res.status(DOCUMENT_NOT_FOUND_ERROR).send({ message: err.message });
-    } else if (err.name === "CastError") {
-      res.status(CAST_ERROR).send({ message: "Invalid data." });
-    } else {
-      res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
-    }
-  });
-}
-
-const unlikeItem = (req, res) => {
-  ClothingItem.findByIdAndUpdate(
-    req.params.itemId,
-    { $pull: { likes: req.user._id }},
-    { new: true }
-  )
-  .orFail()
+    .orFail()
     .then((item) => res.status(200).send({ data: item }))
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
@@ -86,9 +86,32 @@ const unlikeItem = (req, res) => {
       } else if (err.name === "CastError") {
         res.status(CAST_ERROR).send({ message: "Invalid data." });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
       }
     });
-}
+};
+
+const unlikeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => {
+      if (err.name === "DocumentNotFoundError") {
+        res.status(DOCUMENT_NOT_FOUND_ERROR).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        res.status(CAST_ERROR).send({ message: "Invalid data." });
+      } else {
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server." });
+      }
+    });
+};
 
 module.exports = { createItem, getItem, deleteItem, likeItem, unlikeItem };
