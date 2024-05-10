1634a9998044dae57b92c94b2875eef8b60f861d
Tracy Chen <tracy200e@gmail.com>
2024-05-10 10:53:01 +1000

    add: update user route and authentication

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { getUsers, createUser, getUser, logInUser, getCurrentUser } = require("../controllers/users");
+const { getUsers, createUser, getUser, logInUser, getCurrentUser, updateUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
-router.get("/me", getCurrentUser);
+router.get("/me", auth, getCurrentUser);
+router.patch("/me", auth, updateUser);
 
 module.exports = router;
