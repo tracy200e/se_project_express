303bcedb442232890876d628770f63df09ab1606
Tracy Chen <tracy200e@gmail.com>
2024-05-12 17:55:23 +1000

    correct: error message

--- models/user.js
+++ models/user.js
@@ -49,7 +49,7 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
       }
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          return Promise.reject(new Error("Incorrect email or password."));
+          return Promise.reject(new Error("Incorrect email or password"));
         }
         return user;
       });
