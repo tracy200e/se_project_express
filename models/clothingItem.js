360e959abcd8cd9dd81dcac5b7cc960b5e2bb582
Tracy Chen <tracy200e@gmail.com>
2024-11-01 22:08:03 +1100

    update: format

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -2,40 +2,42 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const clothingItemSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 30
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "Link is not valid",
     },
-    weather: {
-        type: String,
-        required: true,
-        enum: ["hot", "warm", "cold"]
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: "",
     },
-    imageUrl: {
-        type: String,
-        required: true,
-        validate: {
-            validator: (v) => validator.isURL(v),
-            message: "Link is not valid"
-        }
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-        required: true
-    },
-    likes: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "user",
-        default: ""
-    }],
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("clothingItem", clothingItemSchema);
