846f68981744dbf88b5ba089ffeedfa453bfa0f1
Tracy Chen <tracy200e@gmail.com>
2025-03-20 11:48:09 +1100

    updated: enable reading of environmental variables

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
