1127527239590891960423a4331a341caa8d58cf
Tracy Chen <tracy200e@gmail.com>
2025-03-22 20:41:15 +1100

    updated: centralization of error handling

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { DOCUMENT_NOT_FOUND_ERROR } = require("../utils/errors");
 const { createUser, logInUser } = require("../controllers/users");
+const NotFoundError = require("../errors/NotFoundError");
 
 const itemRouter = require("./clothingItem");
 const userRouter = require("./users");
@@ -11,8 +11,8 @@ router.post("/signup", createUser);
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 
-router.use((req, res) => {
-  res.status(DOCUMENT_NOT_FOUND_ERROR).send({ message: "Router not found." });
+router.use((req, res, next) => {
+  next(new NotFoundError("Router not found."));
 });
 
 module.exports = router;
