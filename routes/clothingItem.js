6dd6fcaa768f989523289dea75a3dcd32781b10a
Tracy Chen <tracy200e@gmail.com>
2025-03-23 21:19:23 +1100

    updated: applied 'celebrate' validation schemas

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,20 +1,67 @@
 const router = require("express").Router();
-const auth = require("../middlewares/auth");
-const { createItem, getItem, deleteItem, likeItem, unlikeItem } = require("../controllers/clothingItem");
+const { Joi, celebrate } = require("celebrate");
+
+const {
+  createItem,
+  getItem,
+  deleteItem,
+  likeItem,
+  unlikeItem,
+} = require("../controllers/clothingItem");
 
 // Create item
-router.post("/", auth, createItem);
+router.post(
+  "/",
+  celebrate({
+    body: Joi.object().keys({
+      imageUrl: Joi.string().uri.required(),
+      name: Joi.string().required.min(2).max(30),
+      weather: Joi.string().required().validate("hot", "warm", "cold"),
+    }),
+  }),
+  createItem,
+);
 
 // Get item
 router.get("/", getItem);
 
 // Delete item
-router.delete("/:itemId", auth, deleteItem);
+router.delete(
+  "/:itemId",
+  celebrate({
+    body: Joi.object().keys({
+      imageUrl: Joi.string().uri.required(),
+      name: Joi.string().required.min(2).max(30),
+      weather: Joi.string().required().validate("hot", "warm", "cold"),
+    }),
+  }),
+  deleteItem,
+);
 
 // Like item
-router.put("/:itemId/likes", auth, likeItem);
+router.put(
+  "/:itemId/likes",
+  celebrate({
+    body: Joi.object().keys({
+      imageUrl: Joi.string().uri.required(),
+      name: Joi.string().required.min(2).max(30),
+      weather: Joi.string().required().validate("hot", "warm", "cold"),
+    }),
+  }),
+  likeItem,
+);
 
 // Unlike item
-router.delete("/:itemId/likes", auth, unlikeItem);
+router.delete(
+  "/:itemId/likes",
+  celebrate({
+    body: Joi.object().keys({
+      imageUrl: Joi.string().uri.required(),
+      name: Joi.string().required.min(2).max(30),
+      weather: Joi.string().required().validate("hot", "warm", "cold"),
+    }),
+  }),
+  unlikeItem,
+);
 
 module.exports = router;
