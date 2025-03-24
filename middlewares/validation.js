65c36fb137d54ee2c0c7f6789de74f3f73738b3a
Tracy Chen <tracy200e@gmail.com>
2025-03-24 11:40:55 +1100

    corrected: validation schema syntax

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -27,7 +27,7 @@ const validateClothingItem = celebrate({
 
 const validateUserCreation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required.min(2).max(30).messages({
+    name: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
