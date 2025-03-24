fd4662a9662acc11658d687f15ef8f159b6a12f9
Tracy Chen <tracy200e@gmail.com>
2025-03-24 11:39:43 +1100

    updated: validation schemas with celebrate

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
+const { validateUpdatedUser } = require("../middlewares/validation");
 
 router.get("/me", auth, getCurrentUser);
-router.patch("/me", auth, updateUser);
+router.patch("/me", validateUpdatedUser, updateUser);
 
 module.exports = router;
