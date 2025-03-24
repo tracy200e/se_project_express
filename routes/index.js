30881e6edcef96f5c0adba6f11c81df8433fc29f
Tracy Chen <tracy200e@gmail.com>
2025-03-24 11:36:44 +1100

    updated: validation schemas with celebrate

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const { createUser, logInUser } = require("../controllers/users");
 const NotFoundError = require("../errors/NotFoundError");
+const {
+  validateUserCreation,
+  validateUserAuthentication,
+} = require("../middlewares/validation");
 
 const itemRouter = require("./clothingItem");
 const userRouter = require("./users");
 
-router.post("/signin", logInUser);
-router.post("/signup", createUser);
+router.post("/signin", validateUserAuthentication, logInUser);
+router.post("/signup", validateUserCreation, createUser);
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
